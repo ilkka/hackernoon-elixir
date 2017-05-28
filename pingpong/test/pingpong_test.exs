bb25a5c8132d2b95e292973ec89e9292313c781f
Ilkka Poutanen <ilkka@ilkka.io>
2017-05-28 22:43:31 +0300

    Test for a pong when ping

--- pingpong/test/pingpong_test.exs
+++ pingpong/test/pingpong_test.exs
@@ -2,7 +2,16 @@ defmodule PingpongTest do
   use ExUnit.Case
   doctest Pingpong
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  test "it responds to a ping with a pong" do
+    # spawn runs a function from a module with the given args
+    # in a new process, which dies when the function
+    # completes.
+    ping = spawn Ping, :start, []
+    # send a message to a process, the tuple is our message
+    # which contains both a sort of action ("ping") and a
+    # return address (self() returns our PID)
+    send ping, {:ping, self()}
+    # assert that we got a reply
+    assert_receive {:pong, ^ping}
   end
 end
