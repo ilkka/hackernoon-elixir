8e16c55922058f9a1f1ffeccb9f31b026fe8b436
Ilkka Poutanen <ilkka@ilkka.io>
2017-06-05 17:14:01 +0300

    Supervision tree stuffs

---
+++ agent_task_supervision_playground/test/agent_task_supervision_playground_test.exs
@@ -0,0 +1,24 @@
+defmodule AgentTaskSupervisionPlaygroundTest do
+  use ExUnit.Case
+  alias AgentTaskSupervisionPlayground.Bucket
+  doctest AgentTaskSupervisionPlayground
+
+  test "tasks that outlive spawners" do
+    pid = self()
+    spawn(fn() ->
+      Task.Supervisor.start_child(SuperVizier, fn() ->
+        :timer.sleep 50
+        send(pid, :sup)
+      end)
+      Process.exit(self(), :kill)
+    end)
+    :timer.sleep 60
+    assert_receive :sup
+  end
+
+  test "working with an agent" do
+    {:ok, _} = Bucket.start_link(OurBucket)
+    Bucket.push(OurBucket, :foo)
+    assert :foo == Bucket.head(OurBucket)
+  end
+end
