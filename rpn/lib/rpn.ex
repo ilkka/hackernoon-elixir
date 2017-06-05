207859fd234e448a3fbddb55109948765c5420a7
Ilkka Poutanen <ilkka@ilkka.io>
2017-06-05 14:08:11 +0300

    An RPN calculator

---
+++ rpn/lib/rpn.ex
@@ -0,0 +1,40 @@
+defmodule Rpn do
+  @moduledoc """
+  Reverse polish notation calculator
+  """
+
+  def start do
+    {:ok, spawn(__MODULE__, :loop, [[]])}
+  end
+
+  def loop(stack) do
+    receive do
+      {from, ref, :peek} ->
+        send(from, {ref, stack})
+        loop(stack)
+      {:push, :+} ->
+        [second | [first | rest]] = stack
+        loop([first + second | rest])
+      {:push, :-} ->
+        [second | [first | rest]] = stack
+        loop([first - second | rest])
+      {:push, :x} ->
+        [second | [first | rest]] = stack
+        loop([first * second | rest])
+      {:push, val} ->
+        loop([val | stack])
+    end
+  end
+
+  def peek(pid) do
+    ref = make_ref()
+    send(pid, {self(), ref, :peek})
+    receive do
+      {^ref, val} -> val
+    end
+  end
+
+  def push(pid, val) do
+    send(pid, {:push, val})
+  end
+end
