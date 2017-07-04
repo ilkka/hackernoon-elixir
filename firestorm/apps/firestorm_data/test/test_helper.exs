429ea0c6724457e46345ee040f667f237283e873
Ilkka Poutanen <ilkka@ilkka.io>
2017-07-04 23:42:00 +0300

    Switch Ecto Sandbox pool mode to manual
    Makes concurrent testing possible but not child process stuff.

--- firestorm/apps/firestorm_data/test/test_helper.exs
+++ firestorm/apps/firestorm_data/test/test_helper.exs
@@ -1,1 +1,5 @@
 ExUnit.start()
+# When testing, run in manual ownership mode
+# Note that if doing async task stuff this will cause problems
+# (see https://medium.com/@qertoip/making-sense-of-ecto-2-sql-sandbox-and-connection-ownership-modes-b45c5337c6b7)
+Ecto.Adapters.SQL.Sandbox.mode(FirestormData.Repo, :manual)
