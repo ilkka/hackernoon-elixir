7ea22cb8b5526e448b5f5bd18bc921a2eab3205e
Ilkka Poutanen <ilkka@ilkka.io>
2017-07-05 00:06:02 +0300

    Changeset stuff for validation

--- firestorm/apps/firestorm_data/test/firestorm_data/schema/user_test.exs
+++ firestorm/apps/firestorm_data/test/firestorm_data/schema/user_test.exs
@@ -7,7 +7,35 @@ defmodule FirestormData.UserTest do
     end
 
     test "creating a user" do
-        ahto = %User{name: "Ahto Simakuutio"}
-        assert {:ok, _} = Repo.insert(ahto)
+        changeset =
+            %User{}
+            |> User.changeset(%{name: "Ahto Simakuutio",
+                                email: "ahto@simakuut.io"})
+        assert {:ok, _} = Repo.insert(changeset)
+    end
+
+    test "creating a user without an email" do
+        changeset =
+            %User{}
+            |> User.changeset(%{name: "Ahto Simakuutio"})
+        # TODO: silly to match the error message here
+        assert {:email, {"can't be blank", [validation: :required]}} in changeset.errors
+    end
+
+    test "creating a user with an invalid email" do
+        changeset =
+            %User{}
+            |> User.changeset(%{name: "Ahto Simakuutio", email: "invalid"})
+        refute changeset.valid?
+    end
+
+    test "creating two users with the same email" do
+        changeset =
+            %User{}
+            |> User.changeset(%{name: "Ahto Simakuutio",
+                                email: "ahto@simakuut.io"})
+        assert {:ok, _} = Repo.insert(changeset)
+        {:error, new_changeset} = Repo.insert(changeset)
+        assert {:email, {"has already been taken", []}} in new_changeset.errors
     end
 end
