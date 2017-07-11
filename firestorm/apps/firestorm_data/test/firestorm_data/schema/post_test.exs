e5c22031d937f776a3a9a61be70728b23a7d9263
Ilkka Poutanen <ilkka@ilkka.io>
2017-07-11 21:29:45 +0300

    Inserting more posts, querying posts

--- firestorm/apps/firestorm_data/test/firestorm_data/schema/post_test.exs
+++ firestorm/apps/firestorm_data/test/firestorm_data/schema/post_test.exs
@@ -1,20 +1,55 @@
 defmodule FirestormData.PostTest do
   alias FirestormData.{Category, User, Thread, Post, Repo}
   use ExUnit.Case
+  import Ecto.Query
 
   setup do
     :ok = Ecto.Adapters.SQL.Sandbox.checkout(Repo)
     {:ok, category} = %Category{title: "Elixir"} |> Repo.insert
     {:ok, thread} = %Thread{category_id: category.id, title: "Elixir rocks"} |> Repo.insert
-    {:ok, user} = %User{username: "ahto", email: "ahto@simakuut.io"} |> Repo.insert
-    {:ok, category: category, thread: thread, user: user}
+    {:ok, ahto} = %User{username: "ahto", email: "ahto@simakuut.io", name: "Ahto Simakuutio"} |> Repo.insert
+    {:ok, category: category, thread: thread, ahto: ahto}
   end
 
-  test "creating a post", %{thread: thread, user: user} do
+  test "creating a post", %{thread: thread, ahto: ahto} do
     changeset =
       %Post{}
-      |> Post.changeset(%{thread_id: thread.id, body: "Aw yeah", user_id: user.id})
+      |> Post.changeset(%{thread_id: thread.id, body: "Aw yeah", user_id: ahto.id})
     
     assert {:ok, _} = Repo.insert changeset
+  end
+
+  describe "given some posts" do
+    setup [:create_other_users, :create_sample_posts]
+
+    test "finding a post by user", %{firstpost: firstpost, ahto: ahto} do
+      query =
+        from p in Post,
+          where: p.user_id == ^ahto.id
+
+      posts = Repo.all query
+      assert firstpost in posts
+    end
+  end
+
+  defp create_other_users(_) do
+    jorma =
+      %User{username: "jorma", email: "jorma@ter.as", name: "Jorma Teräs"}
+      |> Repo.insert!
+    {:ok, jorma: jorma}
+  end
+
+  defp create_sample_posts(%{thread: thread, ahto: ahto, jorma: jorma}) do
+    first =
+      %Post{}
+      |> Post.changeset(%{thread_id: thread.id, user_id: ahto.id, body: "Quite"})
+      |> Repo.insert!
+    
+    second =
+      %Post{}
+      |> Post.changeset(%{thread_id: thread.id, user_id: jorma.id, body: "Right"})
+      |> Repo.insert!
+
+    {:ok, firstpost: first, secondpost: second}
   end
 end
