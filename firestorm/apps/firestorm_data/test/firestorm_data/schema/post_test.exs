0bd8d92b2d747bd21f2931db698e10782eade55f
Ilkka Poutanen <ilkka@ilkka.io>
2017-07-12 13:54:06 +0300

    Create bigger set of test data

--- firestorm/apps/firestorm_data/test/firestorm_data/schema/post_test.exs
+++ firestorm/apps/firestorm_data/test/firestorm_data/schema/post_test.exs
@@ -22,7 +22,7 @@ defmodule FirestormData.PostTest do
   describe "given some posts" do
     setup [:create_other_users, :create_sample_posts]
 
-    test "finding a post by user", %{firstpost: firstpost, ahto: ahto} do
+    test "finding posts by user", %{posts: [firstpost | _], ahto: ahto} do
       query =
         from p in Post,
           where: p.user_id == ^ahto.id,
@@ -33,13 +33,17 @@ defmodule FirestormData.PostTest do
       assert hd(posts).user.username == "ahto"
     end
 
-    test "counting posts in a thread", %{thread: thread} do
+    test "counting posts in a thread", %{threads: [thread | _]} do
       query =
         from p in Post,
           where: p.thread_id == ^thread.id
 
       postcount = Repo.aggregate(query, :count, :id)
       assert postcount == 2
+    end
+    
+    test "find three most recently updated threads", %{threads: threads} do
+      assert true
     end
   end
 
@@ -50,17 +54,23 @@ defmodule FirestormData.PostTest do
     {:ok, jorma: jorma}
   end
 
-  defp create_sample_posts(%{thread: thread, ahto: ahto, jorma: jorma}) do
-    first =
-      %Post{}
-      |> Post.changeset(%{thread_id: thread.id, user_id: ahto.id, body: "Quite"})
-      |> Repo.insert!
-    
-    second =
-      %Post{}
-      |> Post.changeset(%{thread_id: thread.id, user_id: jorma.id, body: "Right"})
-      |> Repo.insert!
+  defp create_sample_posts(%{category: category, ahto: ahto, jorma: jorma}) do
+    thread1 =
+      %Thread{category_id: category.id, title: "Thread1"} |> Repo.insert!
+    thread1post1 =
+      %Post{thread_id: thread1.id, user_id: ahto.id, body: "Thread1post1"} |> Repo.insert!
+    thread1post2 =
+      %Post{thread_id: thread1.id, user_id: jorma.id, body: "Thread1post2"} |> Repo.insert!
+    thread2 =
+      %Thread{category_id: category.id, title: "Thread2"} |> Repo.insert!
+    thread2post =
+      %Post{thread_id: thread2.id, user_id: ahto.id, body: "Thread2post"} |> Repo.insert!
+    thread3 =
+      %Thread{category_id: category.id, title: "Thread3"} |> Repo.insert!
+    thread3post =
+      %Post{thread_id: thread3.id, user_id: jorma.id, body: "Thread3post"} |> Repo.insert!
 
-    {:ok, firstpost: first, secondpost: second}
+    {:ok, threads: [thread1, thread2, thread3], posts: [thread1post1, thread1post2, thread2post, thread3post]}
   end
+
 end
