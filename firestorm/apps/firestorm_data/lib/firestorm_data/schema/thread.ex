7865373a72c22f253786bbf46729454424ca68b0
Ilkka Poutanen <ilkka@ilkka.io>
2017-07-20 13:40:04 +0300

    Actually working recent threads query

--- firestorm/apps/firestorm_data/lib/firestorm_data/schema/thread.ex
+++ firestorm/apps/firestorm_data/lib/firestorm_data/schema/thread.ex
@@ -14,14 +14,25 @@ defmodule FirestormData.Thread do
   end
 
   def get_recent_threads(category) do
-    alias FirestormData.{Post, Thread, Repo}
+    # alias FirestormData.{Post, Thread, Repo}
     import Ecto.Query
-    from p in Post,
-      order_by: [desc: p.inserted_at],
-      left_join: t in Thread, on: [id: p.thread_id],
-      where: t.category_id == ^category.id,
-      limit: 3,
-      distinct: t.id,
-      select: t
+    # This version doesn't work, perhaps because of
+    # "distinct: t.id" which forces an initial sort by thread
+    # id, or something.
+    # from p in Post,
+    #   order_by: [desc: p.inserted_at],
+    #   left_join: t in Thread, on: [id: p.thread_id],
+    #   where: t.category_id == ^category.id,
+    #   limit: 3,
+    #   distinct: t.id,
+    #   select: t
+    __MODULE__
+      |> join(:left_lateral,
+              [t],
+              p in fragment("SELECT thread_id, inserted_at FROM posts where posts.thread_id = ? ORDER BY posts.inserted_at DESC LIMIT 1", t.id))
+      |> order_by([t, p], [desc: p.inserted_at])
+      |> where(category_id: ^category.id)
+      |> select([t], t)
+      |> limit(3)
   end
 end
